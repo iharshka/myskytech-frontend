c987256d99bdc7b138df911105757d4bcdc9a633
Harsh Gautam <kanhaharsh7725@gmail.com>
2024-08-24 02:21:05 +0530

    Changed form input fields background

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -8,9 +8,9 @@ export default function ContactForm() {
                 Contact Us
             </span>
             <div className="flex flex-col space-y-5 mt-10 justify-center items-center text-neutral-500">
-                <input type="email" required name="email" id="email" placeholder="Email" className="w lg:w-1/3 rounded-full border border-neutral-800 p-2 focus:outline-none placeholder:text-neutral-700" />
-                <input type="tel" required name="tel" id="tel" placeholder="Phone Number" className="w lg:w-1/3 rounded-full border border-neutral-800 p-2 focus:outline-none placeholder:text-neutral-700" />
-                <input type="text" required name="msg" id="msg" placeholder="Enter your message here" className="w lg:w-1/3 rounded-full border border-neutral-800 p-3 focus:outline-none placeholder:text-neutral-700" />
+                <input type="email" required name="email" id="email" placeholder="Email" className="w lg:w-1/3 rounded-full border border-neutral-800 p-2 focus:outline-none placeholder:text-neutral-700 bg-neutral-900" />
+                <input type="tel" required name="tel" id="tel" placeholder="Phone Number" className="w lg:w-1/3 rounded-full border border-neutral-800 p-2 focus:outline-none placeholder:text-neutral-700 bg-neutral-900" />
+                <input type="text" required name="msg" id="msg" placeholder="Enter your message here" className="w lg:w-1/3 rounded-full border border-neutral-800 p-3 focus:outline-none placeholder:text-neutral-700 bg-neutral-900" />
                 <button type="button" className="text-slate-50 bg-gradient-to-r from-indigo-500 to-indigo-700 rounded-full p-2">
                     <Send />
                 </button>
