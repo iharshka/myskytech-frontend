326e5404ad5140075a57c45b31ea66ec8a2afd1d
Harsh Gautam <kanhaharsh7725@gmail.com>
2024-08-25 16:11:11 +0530

    Updated form

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -8,10 +8,15 @@ export default function ContactForm() {
                 Contact Us
             </span>
             <div className="flex flex-col space-y-5 mt-10 justify-center items-center text-neutral-500">
-                <input type="email" required name="email" id="email" placeholder="Email" className="w lg:w-1/3 rounded-full border border-neutral-800 p-2 focus:outline-none placeholder:text-neutral-700 bg-neutral-900" />
-                <input type="tel" required name="tel" id="tel" placeholder="Phone Number" className="w lg:w-1/3 rounded-full border border-neutral-800 p-2 focus:outline-none placeholder:text-neutral-700 bg-neutral-900" />
-                <input type="text" required name="msg" id="msg" placeholder="Enter your message here" className="w lg:w-1/3 rounded-full border border-neutral-800 p-3 focus:outline-none placeholder:text-neutral-700 bg-neutral-900" />
-                <button type="button" className="text-slate-50 bg-gradient-to-r from-indigo-500 to-indigo-700 rounded-full p-2">
+                <input type="text" required name="name" id="name" placeholder='Name'  className="w lg:w-1/3 rounded-full border border-neutral-800 p-2 focus:outline-none placeholder:text-neutral-700 lg:bg-neutral-900" />
+                <input type="email" required name="email" id="email" placeholder="Email" className="w lg:w-1/3 rounded-full border border-neutral-800 p-2 focus:outline-none placeholder:text-neutral-700 lg:bg-neutral-900" />
+                <input type="tel" required name="tel" id="tel" placeholder="Phone Number" className="w lg:w-1/3 rounded-full border border-neutral-800 p-2 focus:outline-none placeholder:text-neutral-700 lg:bg-neutral-900" />
+                <input type="text" required name="msg" id="msg" placeholder="Enter your message here" className="w lg:w-1/3 rounded-full border border-neutral-800 p-3 focus:outline-none placeholder:text-neutral-700 lg:bg-neutral-900" />
+                <div className="flex flex-row space-x-2">
+                    <input type="checkbox" name="optForFollowups" id="optForFollowups" />
+                    <label htmlFor="optForFollowups" className='text-sm'>Subscribe for MST's Offerings & NewsLetter</label>
+                </div>
+                <button type="submit" className="text-slate-50 bg-gradient-to-r from-indigo-500 to-indigo-700 rounded-full p-2">
                     <Send />
                 </button>
             </div>
